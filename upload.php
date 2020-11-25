cc848e4e652270046a9c0e6c6cf3f03fde02e507
PSFRomesh <romesh60@gmail.com>
2020-11-25 18:32:01 +0530

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,40 @@
+<?php
+if(isset($_POST['submit'])){
+    $file=$_FILES['file'];
+    $fileName=$_FILES['file']['name'];
+    //print_r($file);
+   $fileTmpName=$_FILES['file']['tmp_name'];
+    $fileSize=$_FILES['file']['size'];
+    $fileError=$_FILES['file']['error'];
+    $fileType=$_FILES['file']['type'];
+
+    $fileExt = explode('.',$fileName);
+    $fileActualExt=strtolower(end($fileExt));
+    $allowed = array('jpg','jpeg','png','pdf');
+    if (in_array($fileActualExt,$allowed)){
+        if($fileError===0){
+            if($fileSize < 1000000){
+                $fileNameNew=uniqid('', true).".".$fileActualExt;
+                $fileDestination='upload/'.$fileNameNew;
+            move_uploaded_file($fileTmpName,$fileDestination);
+           // header("Location:index.php?uploadsuccess");
+           echo "file uploaded success";
+
+            }else{
+                echo "your file is too big";
+            }
+
+        }else{
+            echo "there is a error in uploading your file";
+        }
+        
+    }
+    else{
+        echo "You cannot upload file of this type";
+    }
+ 
+
+}
+
+
+?>
